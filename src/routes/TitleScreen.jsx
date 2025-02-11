36f1fd92b6be2cad17689ed0e3e7c96402ab5227
Christian <64344406+cjxo@users.noreply.github.com>
2025-02-11 14:07:56 +0800

    Basic Title Screen

--- src/routes/TitleScreen.jsx
+++ src/routes/TitleScreen.jsx
@@ -1,8 +1,69 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+import styles from "../styles/route.module.css";
+import PixelArtCanvas from "../components/PixelArtCanvas";
+
 const TitleScreen = () => {
+  const options = ["Select Game", "Options"];
+  const [optionIdx, setOptionIdx] = useState(0);
+
+  useEffect(() => {
+    const keyListen = (event) => {
+      switch (event.key) {
+        case "ArrowDown": {
+          setOptionIdx((prevIdx) => {
+            const newIdx = (prevIdx + 1) >= options.length ? 0 : prevIdx + 1;
+            console.log("HOO", newIdx);
+            return newIdx;
+          });
+        } break;
+
+        case "ArrowUp": {
+          setOptionIdx((prevIdx) => {
+            const newIdx = (prevIdx - 1) < 0 ? options.length - 1 : prevIdx - 1;
+            console.log("HELLO", newIdx);
+            return newIdx;
+          });
+        } break;
+      }
+    };
+
+    document.addEventListener("keydown", keyListen);
+    return () => document.removeEventListener("keydown", keyListen);
+  }, []);
+
   return (
-    <section>
-      <h1>Arcade Games</h1>
-    </section>
+    <main className={styles.titleScreen}>
+      <div className={styles.center}>
+        <h1>Arcade Games</h1>
+        <ul className={styles.options}>
+          {options.map((option, idx) => (
+            <li key={option}><Link className={(idx === optionIdx) ? styles.selected : ""}>{option}</Link></li>
+          ))}
+        </ul>
+      </div>
+
+      <div className={styles.bottomTip}>
+        <div className={styles.tip}>
+          <PixelArtCanvas
+            alt="arrow keys"
+            imageSrc="./src/assets/textures/navigate.png"
+          />
+          <p>Navigate</p>
+        </div>
+
+        <div className={styles.tip}>
+          <PixelArtCanvas
+            alt="enter"
+            imageSrc="./src/assets/textures/enter.png"
+            scaleX={3}
+            scaleY={3}
+          />
+          <p>Select</p>
+        </div>
+      </div>
+    </main>
   );
 };
 
---
+++ tests/routes/TitleScreen.test.jsx
@@ -0,0 +1,30 @@
+import { render, screen } from "@testing-library/react";
+import TitleScreen from "../../src/routes/TitleScreen";
+import { BrowserRouter } from "react-router-dom";
+
+describe("TitleScreen", () => {
+  it("renders h1 with Arcade Games", () => {
+    render(
+      <BrowserRouter>
+        <TitleScreen />
+      </BrowserRouter>
+    );
+    
+    const heading = screen.getByRole("heading", { name: "Arcade Games" });
+    expect(heading).toBeInTheDocument();
+  });
+
+  it("renders options 'Select Game' and 'Options'", () => {
+    render(
+      <BrowserRouter>
+        <TitleScreen />
+      </BrowserRouter>
+    );
+    
+    const selectGame = screen.getByRole("link", { name: "Select Game" });
+    const options = screen.getByRole("link", { name: "Options" });
+    
+    expect(selectGame).toBeInTheDocument();
+    expect(options).toBeInTheDocument();
+  });
+});
